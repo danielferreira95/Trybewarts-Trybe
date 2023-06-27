d7cdb281f29567af0325b0db819a7760a4722aaa
DanielFerreira <daniel18mtm@gmail.com>
2023-06-27 18:06:44 -0300

    Requisito 1

---
+++ script.js
@@ -0,0 +1,13 @@
+const botao = document.querySelector('button[type="submit"]');
+const entradaEmail = document.getElementById('email');
+const entradaSenha = document.getElementById('senha');
+
+const valida = () => {
+  if (entradaEmail.value === 'tryber@teste.com' || entradaSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+};
+
+botao.addEventListener('click', valida);
